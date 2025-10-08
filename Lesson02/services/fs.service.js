7befeec12f0160230c99f0b7efa1f9dd409ba8ec
TriggeerCat <zspwdmnplay@gmail.com>
2025-10-08 17:28:25 +0300

    feat(L02): add assignment code

---
+++ Lesson02/services/fs.service.js
@@ -0,0 +1,26 @@
+﻿const fs = require('node:fs/promises');
+const path = require('node:path');
+
+const filePath = path.join(process.cwd(), 'db', 'users.json');
+
+const read = async () => {
+    try {
+        const json = await fs.readFile(filePath, 'utf-8');
+        return json ? JSON.parse(json) : [];
+    } catch (e) {
+        return [];
+    }
+};
+
+const write = async (users) => {
+    try {
+        await fs.writeFile(filePath, JSON.stringify(users, null, 2));
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+module.exports = {
+    read,
+    write
+}
---
+++ Lesson02/services/user.service.js
@@ -0,0 +1,30 @@
+﻿const {userRepository} = require('../repositories/user.repository.js');
+const {read, write} = require("./fs.service");
+
+class UserService {
+    async getAllUsers() {
+        return await userRepository.getAllUsers();
+    }
+
+    async getOneUserById(id) {
+        return await userRepository.getOneUserById(id);
+    }
+
+    async createUser(user) {
+        return await userRepository.createUser(user);
+    }
+
+    async updateUser(id, newData){
+        return await userRepository.updateUser(id, newData);
+    }
+
+    async deleteUser(id) {
+        return await userRepository.deleteUser(id);
+    }
+}
+
+const userService = new UserService();
+
+module.exports = {
+    userService
+}
