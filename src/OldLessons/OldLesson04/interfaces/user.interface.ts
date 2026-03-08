5f2e76b710a8754c86215b3860c4f7ebb6f95a19
TriggeerCat <zspwdmnplay@gmail.com>
2026-03-08 15:36:41 +0200

    chore: temporary delete other lessons

--- src/OldLessons/OldLesson04/interfaces/user.interface.ts
+++
@@ -1,9 +0,0 @@
-﻿export interface User {
-    _id: number;
-    name: string;
-    funFact: string;
-    createdAt: Date;
-    updatedAt: Date;
-}
-
-export type UserDTO = Pick<User, "name" | "funFact">;
