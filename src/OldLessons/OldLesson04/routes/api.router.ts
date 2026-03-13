dc71d4db5f1f6745b4136cc896e32deaab2cb8dc
TriggeerCat <zspwdmnplay@gmail.com>
2026-03-13 17:14:23 +0200

    chore: delete old lessons for now

--- src/OldLessons/OldLesson04/routes/api.router.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Router } from "express";
-
-import { userRouter } from "./user.router";
-
-const router = Router();
-
-router.use("/users", userRouter);
-
-export const apiRouter = router;
--- src/OldLessons/OldLesson05/enums/role.enum.ts
+++
@@ -1,6 +0,0 @@
-﻿export const ROLE = {
-    USER: "user",
-    ADMIN: "admin",
-} as const;
-
-export type Role = (typeof ROLE)[keyof typeof ROLE];
