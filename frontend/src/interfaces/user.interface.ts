b62ff4c0a62cdbb5995b3d589404845f8d5bdcdf
TriggeerCat <zspwdmnplay@gmail.com>
2026-03-31 14:07:38 +0300

    feat(L08): add assignment code

---
+++ frontend/src/interfaces/user.interface.ts
@@ -0,0 +1,11 @@
+﻿import {RoleEnum} from "../enums/role.enum";
+
+export interface User {
+    _id: string;
+    name: string;
+    email: string;
+    role: RoleEnum;
+    isDeleted: boolean;
+    isVerified: boolean;
+    isActive: boolean;
+}
